0c72dd1111b723314ba506f3624ffa2fa62d5432
Omar <omar.assadi@drivek.com>
2018-11-26 00:30:44 +0100

    Fixed prod urls

--- src/containers/Root.dev.js
+++ src/containers/Root.dev.js
@@ -9,8 +9,8 @@ const Root = ({ store }) => (
   <Provider store={store}>
     <div className="flexgrid flexgrid-full flexgrid--center flexgrid--middle flexgrid--column u-height--full container">
       <Switch>
-        <Route exact path={`${process.env.PUBLIC_URL}/`} component={CitiesPage} />
-        <Route path={`${process.env.PUBLIC_URL}/:dayId`} component={DayPage} />
+        <Route exact path={`/`} component={CitiesPage} />
+        <Route path={`/:dayId`} component={DayPage} />
       </Switch>
     </div>
   </Provider>
