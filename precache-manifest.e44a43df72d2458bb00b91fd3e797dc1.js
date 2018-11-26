38adf7a2365a9fab23d280586ea2537f04386660
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-26 13:20:57 +0000

    Deploy asso1985/weather-forecast-test to github.com/asso1985/weather-forecast-test.git:gh-pages

---
+++ precache-manifest.e44a43df72d2458bb00b91fd3e797dc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7182e9edb858b27f8b6",
+    "url": "/weather-forecast-test/static/css/main.3dfc3e9a.chunk.css"
+  },
+  {
+    "revision": "b7182e9edb858b27f8b6",
+    "url": "/weather-forecast-test/static/js/main.b7182e9e.chunk.js"
+  },
+  {
+    "revision": "50121ffac60b7ad95e8c",
+    "url": "/weather-forecast-test/static/js/1.50121ffa.chunk.js"
+  },
+  {
+    "revision": "e59faf06766bd074873a",
+    "url": "/weather-forecast-test/static/js/runtime~main.e59faf06.js"
+  },
+  {
+    "revision": "dadd037d252b7cf24c8a3dd127d28384",
+    "url": "/weather-forecast-test/index.html"
+  }
+];
