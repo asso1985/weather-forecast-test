2b3f0cf8a21e29c87b7211b8a66554bf7e623096
Omar <omar.assadi@drivek.com>
2018-11-26 14:03:07 +0100

    Moved Browser History

--- src/containers/Root.prod.js
+++ src/containers/Root.prod.js
@@ -2,17 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import CitiesPage from './CitiesPage';
 import DayPage from './DayPage';
 
+const supportsHistory = 'pushState' in window.history;
+
 const Root = ({ store }) => (
   <Provider store={store}>
+    <BrowserRouter basename={'/weather-forecast-test'} forceRefresh={!supportsHistory}>
     <div className="flexgrid flexgrid-full flexgrid--center flexgrid--middle flexgrid--column u-height--full container">
       <Switch>
         <Route exact path={`/`} component={CitiesPage} />
         <Route path={`/:dayId`} component={DayPage} />
       </Switch>
     </div>
+    </BrowserRouter>
   </Provider>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Root from './containers/Root';
-import { BrowserRouter } from 'react-router-dom';
-import configureStore from './store/configureStore';
 import "./index.scss";
-
+import configureStore from './store/configureStore';
 const store = configureStore();
-const supportsHistory = 'pushState' in window.history
 
 render(
-  <BrowserRouter basename={'/weather-forecast-test'} forceRefresh={!supportsHistory}>
-    <Root store={store} />
-  </BrowserRouter>,
+  <Root store={store} />,
   document.getElementById('root')
 );
