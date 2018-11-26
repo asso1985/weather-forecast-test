2877ded139ddd0e7ee0894f6a626d5821584a72d
Omar <omar.assadi@drivek.com>
2018-11-26 10:46:36 +0100

    Force refresh

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Root from './containers/Root';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store/configureStore';
 import "./index.scss";
 
 const store = configureStore();
+const supportsHistory = 'pushState' in window.history
 
 render(
-  <Router basename={'/weather-forecast-test'}>
+  <BrowserRouter basename={'/weather-forecast-test'} forceRefresh={!supportsHistory}>
     <Root store={store} />
-  </Router>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
